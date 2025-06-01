31439f58d4b744635da12497ea473e0e005ed02e
lucas <llucasaguiar16@gmail.com>
2025-05-31 21:50:54 -0300

    Att

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def ola():
-    # <h1>Olá, mundo!</h1>
+def home():
     return render_template('index.html')
 
 @app.route('/equipe')

dc659d68474c4cc01019db1cc9070d18f9d623b0
lucas <llucasaguiar16@gmail.com>
2025-06-02 04:15:41 -0300

    Att

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request
+import csv
 
 app = Flask(__name__)
 
@@ -10,9 +11,52 @@ def home():
 def equipe():
     return render_template('equipe.html')
 
-@app.route('/dicionario')
-def dicionario():
-    return render_template('dicionario.html')
+@app.route('/glossario')
+def glossario():
+
+    glossario_de_termos = []
+
+    with open('bd_glossario.csv', newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+
+        for t in reader:
+            glossario_de_termos.append(t)
+
+    return render_template('glossario.html', glossario=glossario_de_termos)
+
+@app.route('/novo_termo')
+def novo_termo():
+    return render_template('novo_termo.html')
+
+@app.route('/criar_termo', methods=['POST'])
+def criar_termo():
+
+    termo = request.form['termo']
+    definicao = request.form['definicao']
+
+    with open('bd_glossario.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow([termo, definicao])
+
+    return redirect(url_for('glossario'))
+
+@app.route('/excluir_termo/<int:termo_id>', methods=['POST'])
+def excluir_termo(termo_id):
+
+    with open('bd_glossario.csv', 'r', newline='') as file:
+        reader = csv.reader(file)
+        linhas = list(reader)
+
+    for i, linha in enumerate(linhas):
+        if i == termo_id:
+            del linhas[i]
+            break
+
+    with open('bd_glossario.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(linhas)
+
+    return redirect(url_for('glossario'))
 
 @app.route('/fundamentos')
 def fundamentos():
@@ -22,4 +66,4 @@ def fundamentos():
 def perguntas():
     return render_template('perguntas.html')
 
-app.run()
+app.run(debug=True)
